90ac6eea03f3e32d2845628742ead6515b60b8a5
Mihails Akimenko <mihails.akimenko@gmail.com>
2020-08-31 18:45:06 +0300

    Optimize imports

--- bff/fs-bff-parent/fs-bff-persistence/src/main/java/com/makimenko/fs/persistence/dao/BookDaoMongoImpl.java
+++ bff/fs-bff-parent/fs-bff-persistence/src/main/java/com/makimenko/fs/persistence/dao/BookDaoMongoImpl.java
@@ -4,8 +4,6 @@ import com.makimenko.fs.domain.book.Book;
 import com.makimenko.fs.persistence.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
