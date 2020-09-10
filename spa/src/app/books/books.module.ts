50f491c591d911e13471c9fb9864222563696ae5
Mihails Akimenko <mihails.akimenko@gmail.com>
2020-09-10 15:42:54 +0300

    UI: Integrate search

--- spa/src/app/books/books.module.ts
+++ spa/src/app/books/books.module.ts
@@ -5,16 +5,26 @@ import {BookListComponent} from './book-list/book-list.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from "@angular/material/grid-list";
-import { BookListItemComponent } from './book-list/book-list-item/book-list-item.component';
+import {BookListItemComponent} from './book-list/book-list-item/book-list-item.component';
 import {MatCardModule} from "@angular/material/card";
+import {BookHomeComponent} from './book-home/book-home.component';
+import {BookToolbarComponent} from './book-toolbar/book-toolbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldControl, MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     BookListComponent,
-    BookListItemComponent
+    BookListItemComponent,
+    BookHomeComponent,
+    BookToolbarComponent
   ],
   exports: [
-    BookListComponent
+    BookListComponent,
+    BookHomeComponent
   ],
   imports: [
     CommonModule,
@@ -22,7 +32,12 @@ import {MatCardModule} from "@angular/material/card";
     MatButtonModule,
     MatIconModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ]
 })
 export class BooksModule {
